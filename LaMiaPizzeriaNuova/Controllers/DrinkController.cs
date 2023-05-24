66a769822b6bdd4d48a0a32f6be220453aa6c718
JennyPersichini <nenny1096@gmail.com>
2023-05-24 18:01:03 +0200

    aggiunto tasto modifica

--- LaMiaPizzeriaNuova/Controllers/DrinkController.cs
+++ LaMiaPizzeriaNuova/Controllers/DrinkController.cs
@@ -46,5 +46,56 @@ namespace LaMiaPizzeriaNuova.Controllers
             return View("AddDrink");
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(int Id, Drink drinkModified)
+        {
+            if(ModelState.IsValid)
+            {
+                using(PizzaContext db = new PizzaContext())
+                {
+                    Drink? drinkToModify = db.Bevande.Where(drink => drink.Id == Id).FirstOrDefault();
+
+                    if(drinkToModify != null)
+                    {
+                        drinkToModify.Name = drinkModified.Name;
+                        drinkToModify.Description = drinkModified.Description;
+                        drinkToModify.Liters = drinkModified.Liters;
+                        drinkToModify.Price = drinkModified.Price;
+
+                        db.SaveChanges();
+                        return RedirectToAction("Index");
+                    } else
+                    {
+                        return NotFound("Bevanda non trovata!!!!");
+                    }
+                    
+                }
+               
+            }
+
+            return View(drinkModified);
+        }
+
+        [HttpGet]
+        public IActionResult Update(int Id)
+        {
+            using (PizzaContext db = new PizzaContext())
+            {
+                Drink? drinkToModify = db.Bevande.Where(drink => drink.Id == Id).FirstOrDefault();
+                
+                if(drinkToModify != null)
+                {
+                    return View(drinkToModify);
+                } else
+                {
+                    return NotFound("Bevanda non trovata!!!!");
+                }
+
+            }               
+
+        }
+
     }
+
 }
